034a84c069a024b63badc330f9df3c768cc8239f
adominguez <dmartin.alberto@gmail.com>
2017-12-06 21:00:51 +0100

    feat(html, styles, js): refactor component

--- app-page.js
+++ app-page.js
@@ -20,50 +20,6 @@
         value: null
       },
       /**
-       * Host of url
-       */
-      host: {
-        type: String,
-        value: 'https://adominguez.github.io/'
-      },
-      /**
-       * Host of url
-       */
-      route: {
-        type: String,
-        value: 'bower_components/'
-      },
-      /**
-       * file name
-       */
-      fileName: {
-        type: String,
-        value: null,
-        observer: '_setTypeOfPage'
-      },
-      /**
-       * type of file md | html
-       */
-      fileType: {
-        type: String,
-        value: 'html'
-      },
-      /**
-       * component url
-       */
-      _fileUrl: {
-        type: String,
-        value: null
-      },
-      /**
-       * Show demo or doc if it´s false show demo
-       */
-      showDoc: {
-        type: Boolean,
-        value: false,
-        observer: '_setTypeOfPage'
-      },
-      /**
        * if is true dont show the panel right
        */
       hidePanelRight: {
@@ -84,20 +40,6 @@
         type: Boolean,
         value: false
       }
-    },
-    _setTypeOfPage: function() {
-      var url;
-      var demo = this.showDoc ? 'index.html' : 'demo/index.html';
-      if(this.fileType === 'html') {
-        url = `${this.host}${this.route}${this.fileName}/${demo}`;
-        this.$.zero.hidden = true;
-        this.$.iframe.hidden = false;
-      } else {
-        url = `${this.host}${this.route}${this.fileName}.md`;
-        this.$.zero.hidden = false;
-        this.$.iframe.hidden = true;
-      }
-      this._fileUrl = url;
     }
 
   });
