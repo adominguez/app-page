55a2dadf5b7e241beac79f15fe27c162c4dbd973
adominguez <dmartin.alberto@gmail.com>
2017-12-08 19:08:03 +0100

    feat(html, css, js): refactor, add header, footer, overlay and sidebar

--- app-page.js
+++ app-page.js
@@ -24,23 +24,86 @@
        */
       hidePanelRight: {
         type: Boolean,
-        value: false
+        value: false,
+        reflectToAttribute: true
+      },
+      /**
+       * if is true the header doesn´t show
+       */
+      hideHeader: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+      },
+      /**
+       * if is true the header doesn´t show
+       */
+      hideFooter: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true
+      },
+      /**
+       * if is true the header doesn´t show
+       */
+      hideSidebar: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true
+      },
+      /**
+       * if is true the header shows at 100%
+       */
+      fullHeader: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true
+      },
+      /**
+       * if is true the footer shows at 100%
+       */
+      fullFooter: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true
+      },
+      /**
+       * if is true show the overlay
+       */
+      showOverlay: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true
       },
       /**
        * If is true show an animation to load the page
        */
       fadeIn: {
         type: Boolean,
-        value: false
+        value: false,
+        reflectToAttribute: true
       },
       /**
        * If is true the component is responsive to 770px
        */
       responsive: {
         type: Boolean,
-        value: false
+        value: false,
+        reflectToAttribute: true
       }
-    }
+    },
+
+    /*_setHeader: function(value) {
+      if(value) {
+        this.$.header.style = 'transform: translateY(' + this.$.header.clientHeight * (-1) + 'px)';
+      } else {
+        this.$.header.style = 'transform: translateY(40px)';
+      }
+    },
+
+    _setFooter: function(value) {
+      return value ? this.$.footer.clientHeight  + 'px' : 0;
+    },*/
 
   });
 }());
